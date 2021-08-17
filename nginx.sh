eed5e69320f37fef86f173ad05520142d52fcde3
Nguyễn Hoàng Minh <nghminh163@outlook.com>
2021-08-17 20:45:39 +0700

    Update

--- nginx.sh
+++ nginx.sh
@@ -1,1 +1,3 @@
-docker run -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro nginxproxy/nginx-proxy
+rm -rf nginx
+mkdir nginx
+docker run -d -p 80:80 -p 443:443 -v ./nginx:/etc/nginx/conf.d -v /var/run/docker.sock:/tmp/docker.sock:ro nginxproxy/nginx-proxy
