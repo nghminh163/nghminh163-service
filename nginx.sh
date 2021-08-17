66035245a5093a7e811a21dc6942c4044a66cf0b
Nguyễn Hoàng Minh <nghminh163@outlook.com>
2021-08-17 20:53:55 +0700

    add network

--- nginx.sh
+++ nginx.sh
@@ -1,3 +1,3 @@
 rm -rf nginx
 mkdir nginx
-docker run -d -p 80:80 -p 443:443 -v ./nginx:/etc/nginx/conf.d -v /var/run/docker.sock:/tmp/docker.sock:ro nginxproxy/nginx-proxy
+docker run -d -p 80:80 -p 443:443 -v $PWD:/etc/nginx/conf.d -v /var/run/docker.sock:/tmp/docker.sock:ro nginxproxy/nginx-proxy
