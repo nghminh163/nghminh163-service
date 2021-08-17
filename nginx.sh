25a0f3728faa0a915c82b76b68fdf5dd210af1cb
Nguyễn Hoàng Minh <nghminh163@outlook.com>
2021-08-17 23:38:22 +0700

    Update

--- nginx.sh
+++ nginx.sh
@@ -1,3 +1,3 @@
 rm -rf nginx
 mkdir nginx
-docker run -d -p 80:80 -p 443:443 -v $PWD:/etc/nginx/conf.d -v /var/run/docker.sock:/tmp/docker.sock:ro nginxproxy/nginx-proxy
+docker run -d -p 80:80 -p 443:443 -v $PWD/certs:/etc/nginx/certs -v /var/run/docker.sock:/tmp/docker.sock:ro --name nghminh163-nginx-proxy --net nghminh163-network jwilder/nginx-proxy
